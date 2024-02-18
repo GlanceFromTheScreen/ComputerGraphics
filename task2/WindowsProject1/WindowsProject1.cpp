914e5c3f22d31985624ec7b71ca35596f5ed42b3
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2024-02-18 20:18:27 +0300

    task2 initial

---
+++ task2/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,139 @@
+// WindowsProject1.cpp : Определяет точку входа для приложения.
+//
+#include <windows.h>
+#include <directxcolors.h>
+
+#include "renderer.h"
+
+using namespace DirectX;
+
+HINSTANCE g_hInst = nullptr;
+UINT WindowWidth = 1280;
+UINT WindowHeight = 720;
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+Renderer* pRenderer = nullptr;
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    if (FAILED(InitWindow(hInstance, nCmdShow)))
+        return 0;
+
+    // Main message loop
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        pRenderer->render();
+    }
+    pRenderer->deviceCleanup();
+
+    return (int)msg.wParam;
+}
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow) {
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = 0;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"WindowClass";
+    wcex.hIconSm = 0;
+
+    if (!RegisterClassEx(&wcex))
+        return E_FAIL;
+
+    g_hInst = hInstance;
+    HWND hWnd = CreateWindow(L"WindowClass", L"Hlamkin Evgeniy 5030102/00201",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
+        nullptr, hInstance, nullptr);
+    if (!hWnd) {
+        return E_FAIL;
+    }
+
+    pRenderer = new Renderer();
+    if (!pRenderer->deviceInit(hWnd)) {
+        delete pRenderer;
+        return E_FAIL;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    {
+        RECT rc;
+        rc.left = 0;
+        rc.right = WindowWidth;
+        rc.top = 0;
+        rc.bottom = WindowHeight;
+
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, TRUE);
+
+        MoveWindow(hWnd, 100, 100, rc.right - rc.left, rc.bottom - rc.top, TRUE);
+    }
+
+    return TRUE;
+}
+
+
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
